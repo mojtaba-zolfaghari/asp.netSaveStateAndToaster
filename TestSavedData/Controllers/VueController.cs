2be702eca035bb588cb098752565204962482f27
Mojtaba Zolfaghari <mzolfaghari71@yahoo.com>
2019-08-28 01:10:42 +0430

    First Commit

---
+++ TestSavedData/Controllers/VueController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TestSavedData.Models;
+
+namespace TestSavedData.Controllers
+{
+    public class VueController : Controller
+    {
+        private readonly TestSavedDataContext _context;
+
+        public VueController(TestSavedDataContext context)
+        {
+            _context = context;
+        }
+
+        [Route("/vue")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Person p = new Person() { Id = 1, Name = "moji", age = 27 };
+            //var model = _context.Person;
+            return View();
+        }
+
+        [Route("/vue")]
+        [HttpPost]
+        public IActionResult Post(Person model)
+        {
+            _context.Person.Add(model);
+            _context.SaveChanges();
+
+            return Ok(model);
+        }
+
+        [Route("/vue")]
+        [HttpPut]
+        public IActionResult Put(Person model)
+        {
+            var person = _context.Person.Find(model.Id);
+            person.Name = model.Name;
+            person.age = model.age;
+
+            _context.SaveChanges();
+
+            return Ok(model);
+        }
+
+        [Route("/vue")]
+        [HttpDelete]
+        public IActionResult Delete(Person model)
+        {
+            var person = _context.Person.Find(model.Id);
+            _context.Person.Remove(person);
+
+            _context.SaveChanges();
+
+            return Ok(model);
+        }
+    }
+}
---
+++ TestSavedData/Data/TestSavedDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestSavedData.Models
+{
+    public class TestSavedDataContext : DbContext
+    {
+        public TestSavedDataContext (DbContextOptions<TestSavedDataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestSavedData.Models.Person> Person { get; set; }
+    }
+}
